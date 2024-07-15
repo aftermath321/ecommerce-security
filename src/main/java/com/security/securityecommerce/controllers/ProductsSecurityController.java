757e4f903978687cd0dd16a9417104c21c1298b8
Michał Kiełbasa <77aftermath77@gmail.com>
2024-07-15 16:31:20 -0400

    Added more controllers to service all currently existing endpoints

---
+++ src/main/java/com/security/securityecommerce/controllers/ProductsSecurityController.java
@@ -0,0 +1,32 @@
+package com.security.securityecommerce.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductsSecurityController {
+
+    @GetMapping("/")
+    public String getMany() {
+        return "OK";
+    }
+    @GetMapping("/{id}")
+    public String getOne() {
+        return "OK";
+    }
+    @PostMapping("/")
+    public String post() {
+        return "OK";
+    }
+    @DeleteMapping("/")
+    public String delete() {
+        return "OK";
+    }
+    @PutMapping("/")
+    public String put() {
+        return "OK";
+    }
+
+
+
+}
